f1bc1909af9612f139415de864d9bb7d4564a7e1
hyosup <simpley@naver.com>
2021-07-20 22:13:21 +0900

    20210720 home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+function Food({fav}){
+  return <h1>I love {fav} </h1>
+}
+
 function App() {
-  return <div>안녕하세요.</div>;
+  return (
+    <div>
+      <h1>안녕하세요</h1>
+      <Food fav = "김치" />
+    </div>
+  );
+
 }
 
 export default App;
